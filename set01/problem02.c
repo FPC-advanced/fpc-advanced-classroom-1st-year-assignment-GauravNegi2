e28772a21eb47ab3e95b14b0e54d943717d52243
GauravNegi2 <you@example.com>
2023-10-12 16:32:11 +0530

    committed 10/12/2023, 4:32:11 PM

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,9 @@
 //2. Write a C program to add two numbers.
-
+#include <stdio.h>
+ int main()
+{
+    int x=1,y=2,sum;
+    sum=x+y;
+    printf("the sum is %d",sum);
+    return 0;
+} 
---
+++ set01/problem03.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int input();
+int add(int a, int b);
+void output(int a, int b, int sum);
+int main()
+{
+    int a, b, c, sum;
+    a=input();
+    b=input();
+    sum=add(a,b);
+    output(a, b, sum);
+    return 0;
+
+}
+
+int input()
+{
+    int x;
+    printf("Enter the value\n");
+    scanf("%d", &x);
+    return x;
+}
+
+
+int add(int a, int b)
+{
+    int sum;
+    sum=a+b;
+    return sum;
+}
+
+void output(int a,int b ,int sum)
+{
+    printf("The sum of the %d and %d is %d",a, b, sum);
+}
