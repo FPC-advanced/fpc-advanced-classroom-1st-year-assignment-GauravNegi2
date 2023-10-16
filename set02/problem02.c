b519812993672101ffde9f7dff55548e1148aad7
GauravNegi2 <you@example.com>
2023-10-16 10:08:04 +0530

    committed 10/16/2023, 10:08:04 AM

---
+++ set02/problem02.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int input_side();
+int check_scalene(int a, int b, int c);
+void output(int a, int b, int c, int isscalene);
+
+int main()
+{
+    int a,b,c,isscalene;
+    a=input_side();
+    b=input_side();
+    c=input_side();
+    isscalene=check_scalene(a,b,c);
+    output(a,b,c,isscalene);
+    return 0;
+}
+
+int input_side()
+{
+    int x;
+    printf("Enter the sides: ");
+    scanf("%d",&x);
+    return x;
+}
+
+int check_scalene(int a, int b, int c)
+{
+    if(a==b || a==b || b==c)
+    {
+        return 0;
+    }
+    else 
+    {
+        return 1;
+    }
+}
+
+void output(int a, int b, int c, int isscalene)
+{
+    if(isscalene==0)
+    {
+        printf("The triangle with sides %d, %d and %d is not scalene",a,b,c);
+    }
+    else
+    {
+       printf("It is scalene"); 
+    }
+    
+}
