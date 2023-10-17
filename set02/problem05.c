964dc81e34a7f3461cb48c2aafac875cd67b4531
GauravNegi2 <you@example.com>
2023-10-17 20:30:04 +0530

    committed 10/17/2023, 8:30:03 PM

--- set02/problem05.c
+++ set02/problem05.c
@@ -18,7 +18,16 @@ int input()
 }
 
 int find_gcd(int a, int b)
-{}
+{
+      int gcd;
+    for(int i = 1; i <= a && i <= b; i++) {
+        if(a % i == 0 && b % i == 0) {
+            gcd = i;
+        }
+    }
+    return gcd;
+}
+
 
 void output(int a, int b, int gcd)
 {}
