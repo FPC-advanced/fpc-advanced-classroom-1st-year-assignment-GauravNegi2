846c4199bf666a2ac4d441cfa2e4faa029d76edb
GauravNegi2 <you@example.com>
2023-10-17 20:43:04 +0530

    committed 10/17/2023, 8:43:03 PM

--- set02/problem06.c
+++ set02/problem06.c
@@ -12,6 +12,13 @@ void input_string(char *a)
 
 void str_reverse(char *str, char *rev_str)
 {
+    int i;
+    for(i = 0; str[i] != '\0'; i++);
+    for(int j = 0; j < i; j++) 
+    {
+        rev_str[j] = str[i - j - 1];
+    }
+    rev_str[i] = '\0';
 }
 
 void output(char *a, char *reverse_a)
