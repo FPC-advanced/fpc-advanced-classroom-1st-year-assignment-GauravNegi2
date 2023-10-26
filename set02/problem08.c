e52d77a0a083a41dd4bfef7b6b1cd3a6cb796a53
GauravNegi2 <you@example.com>
2023-10-26 09:53:17 +0530

    committed 10/26/2023, 9:53:16 AM

--- set02/problem08.c
+++ set02/problem08.c
@@ -31,3 +31,9 @@ void find_area(Triangle *t)
 void find_n_areas(int n, Triangle t[n])
 {}
 
+Triangle find_smallest_triangle(int n, Triangle t[n])
+{}
+
+
+void output(int n, Triangle t[n], Triangle smallest)
+{}
