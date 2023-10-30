6815b97c44ab1fe05ce0a087988a4eebeb7effbf
GauravNegi2 <you@example.com>
2023-10-30 10:37:39 +0530

    committed 10/30/2023, 10:37:38 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -31,5 +31,5 @@ float find_distance(float x1, float y1, float x2, float y2)
 
 void output(float x1, float y1, float x2, float y2, float distance)
 {
-    printf("The distance between point (%2.f,%2.f) and (%2.f,%2.f) is %2.f",x1,x2,y1,y2,distance);
+    printf("The distance between point (%2.f,%2.f) and (%2.f,%2.f) is %2.f",x1,y1,x2,y2,distance);
 }
