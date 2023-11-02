e1f67daf365048a9054dd9d2e74e14964c834dd3
GauravNegi2 <you@example.com>
2023-11-02 10:29:45 +0530

    committed 11/2/2023, 10:29:44 AM

---
+++ set03/problem04.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int input();
+int find_fibo(int n);
+void output(int n, int fibo);
+
+int main()
+{
+    int n = input();
+    int fibo = find_fibo(n);
+    output(n, fibo);
+    return 0;
+}
+
+int input()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    return n;
+}
+
+int find_fibo(int n)
+{
+    if(n<=1)
+    {
+        return n;
+    }
+
+    int fibo[n+1];
+    fibo[0]=0;
+    fibo[1]=1;
+
+    for(int i=2;i<=n;i++)
+    {
+        fibo[i]=fibo[i-1]+fibo[i-2];
+    }
+    return fibo[n];
+}
+void output(int n, int fibo)
+{
+    printf("fibo(%d) = %d\n", n, fibo);
+}
