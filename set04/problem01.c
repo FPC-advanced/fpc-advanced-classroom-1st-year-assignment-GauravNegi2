2fa97cdf0d503a90828a4c82288422fe672abdde
GauravNegi2 <you@example.com>
2023-12-05 11:16:07 +0530

    committed 12/5/2023, 11:16:07 AM

--- set04/problem01.c
+++ set04/problem01.c
@@ -21,15 +21,21 @@ void input(int *num1, int *den1, int *num2, int *den2)
     scanf("%d %d",num2,den2);
 }
 
+int find_gcd(int a, int b)
+{
+    for (int temp; b != 0; temp = b, b = a % b, a = temp);
+    return a;
+}
+
 void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den)
 {
     *res_num = num1 * den2 + num2 * den1;
     *res_den = den1 * den2;
     
-    for (int i = *res_den; i > 1; i--) 
-    {
-        (*res_num % i == 0 && *res_den % i == 0) ? (*res_num /= i, *res_den /= i) :0;
-    }
+    int gcd = find_gcd(*res_num, *res_den);
+    *res_num /= gcd;
+    *res_den /= gcd;
+    
 }
 
 void output(int num1, int den1, int num2, int den2, int res_num, int res_den)
